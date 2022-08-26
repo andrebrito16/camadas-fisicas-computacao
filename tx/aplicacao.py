8f9bc244f6ecaa7bb8720eca7b0da024291887e6
Gabriel Valentim <gabriel.valentim.blis@hotmail.com>
2022-08-26 18:23:58 -0300

    TX and RX finish

--- tx/aplicacao.py
+++ tx/aplicacao.py
@@ -29,21 +29,35 @@ serialName = "COM4"                  # Windows(variacao de)
 N = random.randint(10, 30)
 
 commands = {
-    1: b'00 FA 00 00',
-    2: b'00 00 FA 00',
-    3: b'FA 00 00',
-    4: b'00 FA 00',
-    5: b'00 00 FA',
-    6: b'00 FA',
-    7: b'FA 00',
-    8: b'00',
-    9: b'FA'
+    1: b'\x00\xFA\x00\x00\xCC',
+    2: b'\x00\x00\xFA\x00\xCC',
+    3: b'\xFA\x00\x00\xCC',
+    4: b'\x00\xFA\x00\xCC',
+    5: b'\x00\x00\xFA\xCC',
+    6: b'\x00\xFA\xCC',
+    7: b'\xFA\x00\xCC',
+    8: b'\x00\xCC',
+    9: b'\xFA\xCC'
 }
 
+# N = 20
+# num_commands = hex(N)
+# print(len(num_commands), str(num_commands).split('b')[1].split('x')[1].replace('\'',''))
 
-list_commands = [commands[random.randint(1, 9)] for i in range(N)]
+# flagBytes = str(num_commands).split('b')[1].split('x')[1].replace('\'','').upper()
 
-print(list_commands)
+# if len(flagBytes) == 3:
+#     flagBytes = '0' + flagBytes
+# print(flagBytes)
+total_command = b''
+for i in range(N):
+    total_command += commands[random.randint(1, 9)]
+# print(total_command.encode())
+# print(type(len(total_command).to_bytes(1, byteorder='big')))
+# print(len(total_command))
+flag = len(total_command).to_bytes(1, byteorder='big')
+total_command = flag + total_command
+print(N)
 
 def main():
     try:
@@ -62,7 +76,11 @@ def main():
 
         #Se chegamos até aqui, a comunicação foi aberta com sucesso. Faça um print para informar.
         print("Abriu a comunicação")
-        
+
+        # time.sleep(.2)
+        # # com1.sendData(len(num_commands).to_bytes('1', byteorder='big'))
+        # time.sleep(1)
+
            
                   
         #aqui você deverá gerar os dados a serem transmitidos. 
@@ -70,47 +88,56 @@ def main():
         #nome de txBuffer. Esla sempre irá armazenar os dados a serem enviados.
         
         #txBuffer = imagem em bytes!
-        for comm in list_commands:
-            # txBuffer = b'\x12\x13\xAA'  #isso é um array de bytes
-            txBuffer = comm
-        
-            print("meu array de bytes tem tamanho {}" .format(len(txBuffer)))
-            #faça aqui uma conferência do tamanho do seu txBuffer, ou seja, quantos bytes serão enviados.
-        
-                
-            #finalmente vamos transmitir os todos. Para isso usamos a funçao sendData que é um método da camada enlace.
-            #faça um print para avisar que a transmissão vai começar.
-            #tente entender como o método send funciona!
-            #Cuidado! Apenas trasmita arrays de bytes!
-                
-            
-            com1.sendData(np.asarray(txBuffer))  #as array apenas como boa pratica para casos de ter uma outra forma de dados
-            
-            # A camada enlace possui uma camada inferior, TX possui um método para conhecermos o status da transmissão
-            # O método não deve estar fincionando quando usado como abaixo. deve estar retornando zero. Tente entender como esse método funciona e faça-o funcionar.
-            txSize = com1.tx.getStatus()
-            while txSize == 0:
-                txSize = com1.tx.getStatus()
-            print('enviou = {}' .format(txSize))
-            
-            #Agora vamos iniciar a recepção dos dados. Se algo chegou ao RX, deve estar automaticamente guardado
-            #Observe o que faz a rotina dentro do thread RX
-            #print um aviso de que a recepção vai começar.
-            
-            #Será que todos os bytes enviados estão realmente guardadas? Será que conseguimos verificar?
-            #Veja o que faz a funcao do enlaceRX  getBufferLen
-        
-            #acesso aos bytes recebidos
-            txLen = len(txBuffer)
-            # rxBuffer, nRx = com1.getData(txLen)
-            # print("recebeu {} bytes" .format(len(rxBuffer)))
-            
-            # for i in range(len(rxBuffer)):
-            #     print("recebeu {}" .format(rxBuffer[i]))
-            
+        # txBuffer = b'\x12\x13\xAA'  #isso é um array de bytes
 
-                
+        txBuffer = total_command
+    
+        print("meu array de bytes tem tamanho {}" .format(len(txBuffer)))
+        #faça aqui uma conferência do tamanho do seu txBuffer, ou seja, quantos bytes serão enviados.
+    
+            
+        #finalmente vamos transmitir os todos. Para isso usamos a funçao sendData que é um método da camada enlace.
+        #faça um print para avisar que a transmissão vai começar.
+        #tente entender como o método send funciona!
+        #Cuidado! Apenas trasmita arrays de bytes!
+            
         
+        com1.sendData(np.asarray(txBuffer))  #as array apenas como boa pratica para casos de ter uma outra forma de dados
+        
+        # A camada enlace possui uma camada inferior, TX possui um método para conhecermos o status da transmissão
+        # O método não deve estar fincionando quando usado como abaixo. deve estar retornando zero. Tente entender como esse método funciona e faça-o funcionar.
+        txSize = com1.tx.getStatus()
+        while txSize == 0:
+            txSize = com1.tx.getStatus()
+        print('enviou = {}' .format(txSize))
+        
+        #Agora vamos iniciar a recepção dos dados. Se algo chegou ao RX, deve estar automaticamente guardado
+        #Observe o que faz a rotina dentro do thread RX
+        #print um aviso de que a recepção vai começar.
+        
+        #Será que todos os bytes enviados estão realmente guardadas? Será que conseguimos verificar?
+        #Veja o que faz a funcao do enlaceRX  getBufferLen
+    
+        #acesso aos bytes recebidos
+        txLen = 1 #len(txBuffer)
+        flagIsEmpty = com1.rx.getIsEmpty()
+        endtimeStamp = time.time() + 5
+        while flagIsEmpty:
+            flagIsEmpty = com1.rx.getIsEmpty()
+            if time.time() > endtimeStamp:
+                break
+
+        if not flagIsEmpty:
+            time.sleep(1)
+            rxBuffer, nRx = com1.getData(txLen)
+            print("recebeu {} bytes" .format(len(rxBuffer)))
+            for i in range(len(rxBuffer)):
+                print("recebeu {}" .format(rxBuffer[i]))
+        else:
+            print("Não recebeu nada")
+
+            
+    
         # Encerra comunicação
         print("-------------------------")
         print("Comunicação encerrada")
