74d652dc1cbaa54baabd54ada6d80f2fb006906e
André Brito <felipe.brito016@gmail.com>
2022-11-02 22:24:03 -0300

    Project/project 7 (#15)
    * Add project 7
    
    * Init encoder
    
    * Encode
    
    * Encode finish
    
    * Decode Finish
    
    * Finish project 7
    
    Co-authored-by: Gabriel Valentim <gabriel.valentim.blis@hotmail.com>

---
+++ projects/project-7/encode_versaoAlunos.py
@@ -0,0 +1,103 @@
+
+#importe as bibliotecas
+from suaBibSignal import *
+import numpy as np
+import sounddevice as sd
+import matplotlib.pyplot as plt
+import math
+import time
+import suaBibSignal
+
+import sys
+
+#funções a serem utilizadas
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        sys.exit(0)
+
+#converte intensidade em Db, caso queiram ...
+def todB(s):
+    sdB = 10*np.log10(s)
+    return(sdB)
+
+def main():
+    NUM = int(input("Digite o número que você quer teclar (0 a 9): "))
+    signal = suaBibSignal.signalMeu()
+    freqs = {
+        0: [941, 1336],
+        1: [697, 1209],
+        2: [697, 1336],
+        3: [697, 1477],
+        4: [770, 1209],
+        5: [770, 1336],
+        6: [770, 1477],
+        7: [852, 1209],
+        8: [852, 1336],
+        9: [852, 1477],
+        10: [100, 2000]
+    }
+
+    t_list = np.arange(0, 3, 1/44100)
+    A, f1, f2 = 1, freqs[NUM][0], freqs[NUM][1] # Nota lá com aplitude 1
+    sine_f1 = [A * math.sin(2 * np.pi * f1 * t) for t in t_list]
+    sine_f2 = [A * math.sin(2 * np.pi * f2 * t) for t in t_list]
+    # Função para gerar o sinal
+    sine = [sine_f1[i] + sine_f2[i] for i in range(len(sine_f1))]
+    # Reproduzindo o sinal
+    fs = [f1, f2]
+
+    # Duração do sinal
+    duration = 5
+
+
+
+   
+    #********************************************instruções*********************************************** 
+    # seu objetivo aqui é gerar duas senoides. Cada uma com frequencia corresposndente à tecla pressionada
+    # então inicialmente peça ao usuário para digitar uma tecla do teclado numérico DTMF
+    # agora, voce tem que gerar, por alguns segundos, suficiente para a outra aplicação gravar o audio, duas senoides com as frequencias corresposndentes à tecla pressionada, segundo a tabela DTMF
+    # Essas senoides tem que ter taxa de amostragem de 44100 amostras por segundo, entao voce tera que gerar uma lista de tempo correspondente a isso e entao gerar as senoides
+    # Lembre-se que a senoide pode ser construída com A*sin(2*pi*f*t)
+    # O tamanho da lista tempo estará associada à duração do som. A intensidade é controlada pela constante A (amplitude da senoide). Construa com amplitude 1.
+    # Some as senoides. A soma será o sinal a ser emitido.
+    # Utilize a funcao da biblioteca sounddevice para reproduzir o som. Entenda seus argumento.
+    # Grave o som com seu celular ou qualquer outro microfone. Cuidado, algumas placas de som não gravam sons gerados por elas mesmas. (Isso evita microfonia).
+    
+    # construa o gráfico do sinal emitido e o gráfico da transformada de Fourier. Cuidado. Como as frequencias sao relativamente altas, voce deve plotar apenas alguns pontos (alguns periodos) para conseguirmos ver o sinal
+    
+
+    print("Inicializando encoder")
+    print("Aguardando usuário")
+    print("Gerando Tons base")
+    print("Executando as senoides (emitindo o som)")
+    print("Gerando Tom referente ao símbolo : {}".format(NUM))
+    print(f"Gerando frequências : {f1}Hz e {f2}Hz")
+
+    sd.play(sine, 44100)
+
+
+    signal.plotFFT(sine, 44100)
+    # Exibe gráficos
+    plt.xlim(500, 2000)
+    plt.show()
+
+    # Plota o sinal
+    plt.plot(t_list, sine)
+    plt.xlim(0, 0.01)
+    plt.show()
+
+    # Plota spectrograma
+    plt.specgram(sine, Fs=44100, cmap='magma')
+    plt.xlim(0, 3)
+    plt.ylim(0, 5000)
+    plt.show()
+
+    # aguarda fim do audio
+    sd.wait()
+    
+    
+    
+
+if __name__ == "__main__":
+
+    main()
